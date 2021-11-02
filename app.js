9fb1599e6994b97e2f46c7e96a5e4972b4245729
Muhamed younes <mhmduns17@gmail.com>
2021-11-03 00:21:21 +0200

    Modifing redis.js

--- app.js
+++ app.js
@@ -1,58 +1,51 @@
 'use strict';
 
-const express = require('express')
+const express = require('express');
 const app = express();
 const cors = require('cors');
 const bodyPasrser = require('body-parser');
-const config = require('./config');
-const qrcode = require('./Controller/qrcode')
-const prof = require('./routes/Professors_route')
-const announce = require('./routes/announce_route')
-const loadUser = require('./Controller/loadData')
-const subjects = require('./routes/subjects_route')
+const qrcode = require('./Controller/qrcode');
+const prof = require('./routes/Professors_route');
+const announce = require('./routes/announce_route');
+const loadUser = require('./Controller/loadData');
+const subjects = require('./routes/subjects_route');
 const user = require('./routes/user_route');
 const authen = require('./routes/authen_routes');
-
-
 
 // middleware
 app.use(cors());
 app.use(express.json());
 app.use(bodyPasrser.json());
 
-app.get('/', (req,res) => {
-    res.send('its working fine for now!!');
-})
+app.get('/', (req, res) => {
+  res.send('its working fine for now!!');
+});
 
 // proffesors
 app.use('/prof', prof.routes);
 
 // announcements
-app.use('/announce', announce.routes)
-
+app.use('/announce', announce.routes);
 
 // ===========================
 
 // AUTH engin for register, login and forgotPassword
-app.use('/auth', authen.routes)
+app.use('/auth', authen.routes);
 
 // post request to load the user from the database
-app.post('/loaduser', loadUser.loadUser)
+app.post('/loaduser', loadUser.loadUser);
 
 // post request for user qrcode
-app.post('/qrcode', qrcode.handleUuid)
+app.post('/qrcode', qrcode.handleUuid);
 
 // ===============================================
 
 // use a middleware for user subjects routes
-app.use('/sub', subjects.routes)
+app.use('/sub', subjects.routes);
 
 // ================================
 // use a middleware for user modifying routes
-app.use('/modify', user.routes)
+app.use('/modify', user.routes);
 
-const port = config.port || '5000'
-app.listen(port, ()=> console.log(`app is running on port ${port}`));
-
-
-
+const port = process.env.port || '3000';
+app.listen(port, () => console.log(`app is running on port ${port}`));
--- redis.js
+++ redis.js
@@ -1,24 +1,20 @@
 var redis = require('redis');
 var url = require('url');
 
-// const {redisConfig} = require('./config');
+const { redisConfig } = require('./config');
 
 // authenticate to redis cli & hosting
-// const client = redis.createClient(
-//     redisConfig.port,
-//      redisConfig.host
-//    );
-//    client.auth(redisConfig.auth);
+const client = redis.createClient(redisConfig.port, redisConfig.host);
+client.auth(redisConfig.auth);
 
-//   // checking the connectivity
-//  client.on("error", function(error) {
-//    console.error(error);
-//  });
+// checking the connectivity
+client.on('error', function (error) {
+  console.error(error);
+});
 
-
-var redisURL = url.parse(process.env.REDISCLOUD_URL);
-var client = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
-client.auth(redisURL.auth.split(":")[1]);
+// var redisURL = url.parse(process.env.REDISCLOUD_URL);
+// var client = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
+// client.auth(redisURL.auth.split(":")[1]);
 
 // check the host by setting a value
 // when the server is on redis should be on as well
